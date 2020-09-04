553da28038be9ed2ad5e0e7b66092641d1863c40
Juan <49132815+juanccassano@users.noreply.github.com>
2020-09-03 21:28:26 -0300

    Datos del primer formulario identificados y guardados en memoria en el segundo, metodo de guardado y redireccion a lista de contactos

--- app/src/main/java/utn/frgp/tusi/tp2_grupo_7/AddContactMore.java
+++ app/src/main/java/utn/frgp/tusi/tp2_grupo_7/AddContactMore.java
@@ -1,13 +1,19 @@
 package utn.frgp.tusi.tp2_grupo_7;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class AddContactMore extends OptionMenu {
 
     private RadioButton primarioInc, primarioCom, secundarioInc, secundarioCom, otroEstudio;
     private CheckBox deporte, arte, musica, tecnologia;
+    private String nombre, apellido, telefono, tipo_telefono, tipo_email, email, direccion, fechnac;
 
 
     @Override
@@ -25,6 +31,43 @@ public class AddContactMore extends OptionMenu {
         arte = (CheckBox) findViewById(R.id.chk_arte);
         musica = (CheckBox) findViewById(R.id.chk_musica);
         tecnologia = (CheckBox) findViewById(R.id.chk_tecnologia);
+
+        String nombre = getIntent().getStringExtra("nombre");
+        String apellido = getIntent().getStringExtra("apellido");
+        String telefono = getIntent().getStringExtra("telefono");
+        String tipo_telefono = getIntent().getStringExtra("tipo_telefono");
+        String tipo_email = getIntent().getStringExtra("tipo_email");
+        String email = getIntent().getStringExtra("email");
+        String direccion = getIntent().getStringExtra("direccion");
+        String fechnac = getIntent().getStringExtra("fechnac");
+
+
+
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+
     }
 
+
+
+    public void guardarYFinalizar(View view){
+        //método para crear contacto
+
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor Obj_editor = preferences.edit();
+        Obj_editor.putString("nombre", nombre);
+        Obj_editor.putString("apellido", apellido);
+        Obj_editor.putString("telefono", telefono);
+        Obj_editor.putString("tipo_email", tipo_email);
+        Obj_editor.putString("email", email);
+        Obj_editor.putString("direccion", direccion);
+        Obj_editor.putString("fechnac", email);
+
+        //agregar segunda pantalla de datos
+
+        Obj_editor.commit();
+        Toast.makeText(this, "El contacto fue guardado con éxito", Toast.LENGTH_SHORT).show();
+
+        Intent fin = new Intent(this, ListContacts.class);
+        startActivity(fin);
+    }
 }
