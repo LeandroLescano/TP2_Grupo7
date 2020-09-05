5f5d39453e5cefa0061e3344d4edfefff69e5e55
Franco Gallardo <40015749+FGalla456@users.noreply.github.com>
2020-09-04 23:47:20 -0300

    Actualizacion
    Actualizo el TP

--- app/src/main/java/utn/frgp/tusi/tp2_grupo_7/AddContactMore.java
+++ app/src/main/java/utn/frgp/tusi/tp2_grupo_7/AddContactMore.java
@@ -3,18 +3,31 @@ package utn.frgp.tusi.tp2_grupo_7;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.util.Output;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class AddContactMore extends OptionMenu {
 
+    private static final String FILE_NAME = "example.txt";
     private RadioButton primarioInc, primarioCom, secundarioInc, secundarioCom, otroEstudio;
     private CheckBox deporte, arte, musica, tecnologia;
     private String nombre, apellido, telefono, tipo_telefono, tipo_email, email, direccion, fechnac;
-
+    EditText name;
+    EditText surname;
+    EditText gmail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,15 +54,16 @@ public class AddContactMore extends OptionMenu {
         String direccion = getIntent().getStringExtra("direccion");
         String fechnac = getIntent().getStringExtra("fechnac");
 
-
-
-        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        name = (EditText)findViewById(R.id.et_nombre);
+        surname = (EditText)findViewById(R.id.et_apellido);
+        gmail = (EditText)findViewById(R.id.et_email);
+        //SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
 
     }
 
 
 
-    public void guardarYFinalizar(View view){
+   /* public void guardarYFinalizar(View view){
         //método para crear contacto
 
         SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
@@ -69,5 +83,32 @@ public class AddContactMore extends OptionMenu {
 
         Intent fin = new Intent(this, ListContacts.class);
         startActivity(fin);
+    }*/
+
+    public void guardarYFinalizar(View v) {
+        String nombre = getIntent().getStringExtra("nombre");
+        String apellido = getIntent().getStringExtra("apellido");
+        String email = getIntent().getStringExtra("email");
+        String text = nombre + " " + apellido + " - " + email + "\n";
+        FileOutputStream fos = null;
+        try {
+            OutputStreamWriter textoalmacenado = new OutputStreamWriter(openFileOutput(FILE_NAME, MODE_PRIVATE));
+            textoalmacenado.write(text);
+            textoalmacenado.flush();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/utn/frgp/tusi/tp2_grupo_7/ListContacts.java
+++ app/src/main/java/utn/frgp/tusi/tp2_grupo_7/ListContacts.java
@@ -1,20 +1,58 @@
 package utn.frgp.tusi.tp2_grupo_7;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class ListContacts extends OptionMenu {
+    private static final String FILE_NAME = "example.txt";
+    private ListView Lista;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_contacts);
+        Lista = (ListView)findViewById(R.id.lvcontactos);
+        load();
     }
 
+    public void load() {
+        FileInputStream fis = null;
+        ArrayList<String> Contactos = new ArrayList<>();
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+            Contactos.add(sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        ArrayAdapter adap = new ArrayAdapter(this,android.R.layout.simple_list_item_1,Contactos);
+        Lista.setAdapter(adap);
+    }
+    
 }
